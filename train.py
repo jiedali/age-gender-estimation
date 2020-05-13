17e8528a49c8588b9d7b2554949e0fd6c50cc913
Jieda Li <jiedali2020@u.northwestern.edu>
2020-05-13 15:14:31 -0500

    debug epoch=2

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ def get_args():
                         help="path to input database mat file")
     parser.add_argument("--batch_size", type=int, default=32,
                         help="batch size")
-    parser.add_argument("--nb_epochs", type=int, default=30,
+    parser.add_argument("--nb_epochs", type=int, default=3,
                         help="number of epochs")
     parser.add_argument("--lr", type=float, default=0.1,
                         help="initial learning rate")
